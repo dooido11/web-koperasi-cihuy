49437113a828732441d79a830b7d2c86dbc5c4f5
dooido11 <ridhooraamadhan70@gmail.com>
2025-01-07 17:55:24 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const contactButton = document.getElementById('contactButton');
+
+    // Menambahkan event listener untuk tombol "Kontak Kami"
+    contactButton.addEventListener('click', function() {
+        alert('Terima kasih telah menghubungi kami! Kami akan segera merespons.');
+    });
+});
